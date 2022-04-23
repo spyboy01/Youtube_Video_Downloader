e5a701e05b0301cf53e25d9c8e4aa365fda38706
Sandeep Singh <sandeepsingh.nita@gmail.com>
2022-04-23 23:43:24 +0530

    adding exception handler and important library
    adding exception handler and important library

---
+++ source_code.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 2 13:33:03 2022
+
+@author: sandeepsingh
+"""
+from cmath import e
+import imp #importing imp to access the import internals
+import os
+import logging
+from tkinter import E
+#import pytube
+import urllib
+import datetime
+import sys
+try:
+    import pytube
+    except ImportError as e
+    print ("Getting import error as Imported module doesn't exist", e)
+
+
+    
+
+
+def download_video():
+    URL= input("Emter The YouTube Video Link")
+
+
+
+
+
+
