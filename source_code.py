87983067da050faac4bf7a9c4e7749bdddb6b5bf
Sandeep Singh <sandeepsingh.nita@gmail.com>
2022-04-24 00:23:15 +0530

    written download_video():
    Written download_video function to download the video

--- source_code.py
+++ source_code.py
@@ -19,11 +19,40 @@ try:
     print ("Getting import error as Imported module doesn't exist", e)
 
 
-    
+
 
 
 def download_video():
-    URL= input("Emter The YouTube Video Link")
+    URL= input("Enter The YouTube Video Link")
+    try:
+        video_link=YouTube(URL)
+        print("Video Title is:",video_link.title)
+        val=input("Do you want to download ? Y/N \n")
+        if(val=='Y'): #if the user input is yes
+            #video_link=video_link.streams.first()
+            video_link=video_link.streams.get_highest_resolution()
+            video_link.download()
+            print("Dowanloading started", video_link.title)
+            #directory path to download videos
+            video_link.download("C:\learning\Pythin_practice\Download")
+            print ("video downloaded succesfully")
+            return 0
+
+        else:
+            return 0
+    except:
+        print("error occur while downloading the video")
+        for i in range(5):
+            time.sleep(2)
+            print("System will retry after 2 second")
+
+# Main drama will start from here.            
+
+
+
+
+
+
 
 
 
